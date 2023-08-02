9c9f0ed30a847e655e2fb79aa98eede26ecaa318
Sergiy <33hantkun33@gmail.com>
2023-08-02 15:42:55 +0300

    add cicle in movieDB

--- movieDB.js
+++ movieDB.js
@@ -1,5 +1,6 @@
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
+
 const personalMovieDB = {
     count : numberOfFilms,
     movies : {},
@@ -7,13 +8,30 @@ const personalMovieDB = {
     genres : [],
     privat : false
 }
+if(personalMovieDB.count < 10){
+    console.log("Посмотрено довольно мало фильмов");
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count <30 ){
+    console.log("Вы клсический киноман")
+}else if(personalMovieDB.count >=30 && personalMovieDB.count<= 60){
+    console.log("Вы киноман");
+}else{
+    console.log("Eror");
+}
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for(let i = 0; i< 2; i++){
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
+
+    if(a === "" || a == null || a.length > 50 ){
+        i--;
+    }
+    else{
+        personalMovieDB.movies[a] = b;
+    }
+}
+
+
+
 
 console.log(personalMovieDB)
